8d76b2faa5db3bf10ae94a3dbe12b34b35bc3dd4
Alishba <alishbashazadi013@gmail.com>
2025-07-27 01:44:02 +0500

    updated git push

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,10 @@
-import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
 import react from "@vitejs/plugin-react";
-import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    ...(mode === "development" ? [screenGraphPlugin()] : [])
-  ],
-  publicDir: "./static",
-  base: "./",
-  css: {
-    postcss: {
-      plugins: [tailwind()],
-    },
-  },
+export default defineConfig({
+  plugins: [react()],
   server: {
-    fs: {
-      strict: false
-    },
-    hmr: {
-      overlay: false
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-}));
+    port: 3000,
+    host: true
+  }
+});
